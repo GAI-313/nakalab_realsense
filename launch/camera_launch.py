ff7eafaa919263f840d9b940b61ad3130f1ed899
GAI-313 <nakatogawagai@gmail.com>
2025-03-28 21:07:34 +0900

    デモ用Xacro追加

--- launch/camera_launch.py
+++ launch/camera_launch.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -16,6 +18,11 @@ def generate_launch_description():
         get_package_share_directory('nakalab_realsense'),
         'params', 'd435.yaml'
     )
+    default_xacro = os.path.join(
+        get_package_share_directory('nakalab_realsense'),
+        'urdf', 'd435.urdf.xacro'
+    )
+
 
 
     declare_params_file = DeclareLaunchArgument(
@@ -30,10 +37,20 @@ def generate_launch_description():
         'namespace', default_value='',
         description='親名前空間'
     )
+    declare_xacro_file = DeclareLaunchArgument(
+        'xacro_file', default_value=default_xacro,
+        description='デモ用カメラの Xacro モデルファイルの絶対パス'
+    )
+    declare_use_description = DeclareLaunchArgument(
+        'use_description', default_value='False',
+        description='デモ用のカメラモデルを使用し、Rviz2 を起動する'
+    )
 
     ld.add_action(declare_params_file)
     ld.add_action(declare_camera_name)
     ld.add_action(declare_namespace)
+    ld.add_action(declare_xacro_file)
+    ld.add_action(declare_use_description)
 
 
     node_realsense = Node(
@@ -48,8 +65,37 @@ def generate_launch_description():
         emulate_tty=True,
         namespace=LaunchConfiguration('namespace')
     )
+    node_demo_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d',
+            os.path.join(
+                get_package_share_directory('nakalab_realsense'),
+                'rviz', 'display.rviz'
+            )
+        ],
+        condition=IfCondition(LaunchConfiguration('use_description'))
+    )
 
     ld.add_action(node_realsense)
+    ld.add_action(node_demo_rviz)
+
+
+    launch_demo_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('nakalab_realsense'),
+                'launch', 'display_launch.py'
+            )
+        ]),
+        launch_arguments={
+            'xacro_file': LaunchConfiguration('xacro_file'),
+            'camera_name': LaunchConfiguration('camera_name')
+        }.items(),
+        condition=IfCondition(LaunchConfiguration('use_description'))
+    )
+
+    ld.add_action(launch_demo_description)
 
 
     return ld
